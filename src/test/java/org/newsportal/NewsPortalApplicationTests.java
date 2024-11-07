4af9819afeb807ba45b082bfd6900b3a3f4c2a2e
Dimitri Diel <150340219+DimitriDiel@users.noreply.github.com>
2024-11-07 17:43:11 +0400

    Initial commit

---
+++ src/test/java/org/newsportal/NewsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.newsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
